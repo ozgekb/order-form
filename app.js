21f7e73c21ba04904cd6e6b54d9bbcc7a2f8c8f5
ozge kantar boyraz <ozge.kantar@gmail.com>
2017-08-24 20:57:40 -0700

    data is added to local storage

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 'use strict';
 
-function Product (imageName, filePath, imageId, productQuantity){
-  this.imageName = imageName;
+function Product (productName, filePath, productId, productQuantity){
+  this.productName = productName;
   this.filePath = filePath;
-  this.imageId = imageId;
-  this.numShown = 0;
-  this.numClicked = 0;
-  this.productQuantity = 0;
+  this.productId = productId;
+  this.productQuantity =  productQuantity;
 }
 var imageFileList = ['bag.jpg','banana.jpg','bathroom.jpg','boots.jpg',
   'breakfast.jpg','bubblegum.jpg','chair.jpg','cthulhu.jpg','dogDuck.jpg',
@@ -14,32 +12,44 @@ var imageFileList = ['bag.jpg','banana.jpg','bathroom.jpg','boots.jpg',
   'sweep.png','tauntaun.jpg','usb.gif','waterCan.jpg','wineGlass.jpg'];
 
 
-  var cartArray = [];
 
-  if(localStorage.getItem('haveItem')) {
-   cartArray = JSON.parse(localStorage.getItem('haveItem'));
- }else{
-    cartArray = new Product(dropDropdownMenu.value, 'img/' + dropDropdownMenu +'.jpg' ,dropDropdownMenu.selectedIndex, productQuantity);
- }
+//  var menu = document.getElementById('product-dropdown');
+  //id = menu.options[menu.selectedIndex].value;
+  //console.log(id);
+  //name  = menu.options[menu.value];
+  //console.log(name);
+  //quantity = parseInt(this.elements['productQuantity'].value);
+//  console.log(quantity);
+
+
+
+function addToCart(product){
+  if (localStorage.getItem('cart') === null){
+    var cart = [];
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+  cart = JSON.parse(localStorage.getItem('cart'));
+  cart.push(product);
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function processForm(event){
+  event.preventDefault();
   console.log(event);
   console.log(this);
   var dropDropdownMenu = document.getElementById('product-dropdown');
-  //console.log(dropDropdownMenu);
+  var id = dropDropdownMenu.selectedIndex;
   console.log(dropDropdownMenu.selectedIndex);
+  var name = dropDropdownMenu.value;
   console.log(dropDropdownMenu.value);
   var productQuantity = parseInt(this.elements['productQuantity'].value);
-  console.log(productQuantity);
-  cartArray.push(dropDropdownMenu,productQuantity);
 
-
-function processForm(event){
-  event.preventDefault();//stop form from actually sending data elsewher
-  //imageList = JSON.parse(localStorage.main);
-  //theForm.reset();
+  var newProduct = new Product(name, 'img/' + name ,id, productQuantity);
+  addToCart(newProduct);
 }
+
+
 var form = document.getElementById('orderForm');
 var subButton = document.getElementById('submit-button');
-console.log(subButton);
-form.addEventListener('submit', processForm);
 
-localStorage.setItem('haveItem',JSON.stringify(cartArray));
+form.addEventListener('submit', processForm);
