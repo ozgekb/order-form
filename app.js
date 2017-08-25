dcb619d81042394737ca11f2d1bddad90108ec1c
ozge kantar boyraz <ozge.kantar@gmail.com>
2017-08-24 17:53:19 -0700

    no proggress

--- app.js
+++ app.js
@@ -13,28 +13,33 @@ var imageFileList = ['bag.jpg','banana.jpg','bathroom.jpg','boots.jpg',
   'dragon.jpg','pen.jpg','petSweep.jpg','scissors.jpg','shark.jpg','unicorn.jpg',
   'sweep.png','tauntaun.jpg','usb.gif','waterCan.jpg','wineGlass.jpg'];
 
-var imageList = [];
-if (localStorage.getItem('main')) {
-  imageList = JSON.parse(localStorage.main);
-} else {
-  for (var i = 0; i < imageFileList.length; i++){
-    var newProduct = new Product(imageFileList[i], 'img/' + imageFileList[i], i);
-    imageList.push(newProduct);
-  }
-}
 
-function processForm(event){
-  event.preventDefault();//stop form from actually sending data elsewher
+  var cartArray = [];
+
+  if(localStorage.getItem('haveItem')) {
+   cartArray = JSON.parse(localStorage.getItem('haveItem'));
+ }else{
+    cartArray = new Product(dropDropdownMenu.value, 'img/' + dropDropdownMenu +'.jpg' ,dropDropdownMenu.selectedIndex, productQuantity);
+ }
   console.log(event);
   console.log(this);
   var dropDropdownMenu = document.getElementById('product-dropdown');
   //console.log(dropDropdownMenu);
   console.log(dropDropdownMenu.selectedIndex);
+  console.log(dropDropdownMenu.value);
   var productQuantity = parseInt(this.elements['productQuantity'].value);
   console.log(productQuantity);
+  cartArray.push(dropDropdownMenu,productQuantity);
+
+
+function processForm(event){
+  event.preventDefault();//stop form from actually sending data elsewher
+  //imageList = JSON.parse(localStorage.main);
   //theForm.reset();
 }
 var form = document.getElementById('orderForm');
 var subButton = document.getElementById('submit-button');
 console.log(subButton);
 form.addEventListener('submit', processForm);
+
+localStorage.setItem('haveItem',JSON.stringify(cartArray));
